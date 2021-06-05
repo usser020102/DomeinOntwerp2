530e556b0073143c56341452a8570bd4d5ca8ffe
ky20096399 <20096399@student.hhs.nl>
2021-06-05 17:38:43 +0200

    Initial commit

---
+++ test/StartschermTest.java
@@ -0,0 +1,36 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StartschermTest {
+    /**
+     * Het is mij niet gelukt om JUnit test te maken voor een arraylist die leeg is, en dan daarna met user input testen of er een inhoud is.
+     * Ook weet ik niet hoe je precies een case kan testen met methodes als switches.
+     *
+     */
+
+    @Test
+    void menu() {
+
+    }
+
+    @Test
+    void addTeam() {
+
+    }
+
+    @Test
+    void printTeam() {
+
+    }
+
+    @Test
+    void printFAQ() {
+    }
+
+
+
+}
